e2ebc0d3c8b5ec94484ec41f9157f379ce72191d
babyli_glb <babyli_glb@hundsun.com>
2018-10-10 14:45:05 +0800

    完成购物车列表

---
+++ src/components/GoodsButton/GoodsButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <!--<div class="button-group">
+    <button class="button-left" type="button">一</button>
+    <input type="number" disabled="disabled" class="button-number" :value="count">
+    <button class="button-right">十</button>
+  </div>-->
+  <div class="van-sku__stepper van-stepper">
+    <button class="van-stepper__minus"></button>
+    <input type="number" disabled="disabled" class="van-stepper__input" value="2">
+    <button class="van-stepper__plus"></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 3,
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .button-group {
+    float: right;
+    padding: 0;
+    margin: 0;
+    display: block;
+  }
+  // 左边和右边的button
+  .button-left, .button-right {
+    border-radius: 2px 0 0 2px;
+    margin: 0;
+    font-family: inherit; // 使用指定给父元素的所有值”。如果父元素中没有明确的值定义，浏览器搜寻DOM树直到找到相应的属性。最终无法找到的话，它会使用浏览器的默认值
+    font-size: 0.8rem;
+    color: inherit;
+    width: 40px;
+    height: 30px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border: 1px solid #eee;
+    position: relative;
+    /*padding: 5px;*/
+    vertical-align: middle;
+  }
+  // 显示的数量
+  .button-number[disabled] {
+    color: #c9c9c9;
+    background-color: #f8f8f8;
+    width: 33px;
+    height: 26px;
+    border: 1px solid #eee;
+    border-width: 1px 0;
+    border-radius: 0;
+    -webkit-box-sizing: content-box;
+    color: #666;
+    font-size: 14px;
+    vertical-align: middle;
+    text-align: center;
+    -webkit-appearance: none;
+  }
+</style>
--- src/pages/ShopCart/ShopCart.vue
+++ src/pages/ShopCart/ShopCart.vue
@@ -2,17 +2,65 @@
   <div class="container-fluid" style="overflow:scroll; ">
     <!--首页头部-->
     <HeaderTop title="我的购物车">
-      <span name="right">管理</span>
+      <div class="header_login" slot="right">
+        <span class="header_login_text">
+           <span @click="showDelete()">管理</span>
+        </span>
+      </div>
     </HeaderTop>
+    <!--<p style="padding-top: 0.8rem; background-color: #FF5500"" >共5件商品</p>-->
+    <div class="shopCart-header">
+      <div class="row">
+        <div class="col-12" style="background-color: #FF5500">
+          <p style="padding-top: 0.8rem">共5件宝贝</p>
+        </div>
+      </div>
+    </div>
+    <div class="shopCart-list">
+      <hr/>
+      <div class="row">
+        <div class="col-2" align="right">
+          <van-checkbox ></van-checkbox>
+        </div>
+        <div class="col-8" align="left">
+          <i class="iconfont icon-fanhui"></i>
+          <span align="left">顾大嫂旗舰店 ></span>
+        </div>
+        <div class="col-2">
+          <p align="left">领卷</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2" align="right">
+          <van-checkbox ></van-checkbox>
+        </div>
+        <div class="col-4" align="left">
+          <img src="./img/shop1.png" width="100%"/>
+        </div>
+        <div class="col-6" align="left">
+          <p style="font-size: 0.8rem">酸辣粉5袋装，正宗重庆酸辣粉酸辣粉5袋装...</p>
+          <span style="text-align: left; font-size: 0.8rem; color: red">￥29.9</span>
+
+          <GoodsButton/>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-  import HeaderTop from '@/components/HeaderTop/HeaderTop.vue'
+  import HeaderTop from '@/components/HeaderTop/HeaderTop.vue';
+  import GoodsButton from '@/components/GoodsButton/GoodsButton.vue';
+  import { Checkbox, Sku } from 'vant';
   export default {
 
     components: {
       HeaderTop,
+      GoodsButton,
+      [Checkbox.name]: Checkbox,
+      [Sku.name]: Sku,
     }
   }
 </script>
